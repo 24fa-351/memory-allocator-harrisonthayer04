d4f585bc4eb4c0effed9dd48c097773704c763cd
harrisonthayer <harrisonthayer04@gmail.com>
2024-11-26 09:53:35 -0800

    Added heap

--- malloc.c
+++ malloc.c
@@ -1,4 +1,5 @@
 #include "malloc.h"
+#include "heap.h"
 
 #include <stdbool.h>
 #include <stdio.h>
@@ -11,29 +12,42 @@ typedef struct memoryBlock {
     bool blockIsFree;
 } memoryBlock;
 
-#define MAX 17592186044416
+#define MAX 1000000
 
 static memoryBlock blocks[MAX];
 static int numberOfBlocks = 0;
 
+void* getLargerBlock(size_t blockSize) {
+    if (numberOfBlocks >= MAX) {
+        fprintf(stderr, "Error: Maximum number of memory blocks reached\n");
+        return NULL;
+    }
+    void* newPtrToData = sbrk(blockSize);
+    blocks[numberOfBlocks].ptrToData = newPtrToData;
+    blocks[numberOfBlocks].blockSize = blockSize;
+    blocks[numberOfBlocks].blockIsFree = false;
+    numberOfBlocks++;
+    return newPtrToData;
+}
+
 // Merges two adjacent blocks if they are both free, ensuring that the memory in
 // our array doesn't get fragmented.
 void mergeBlocks() {
-    for (int i = 0; i < numberOfBlocks - 1;) {
-        if (blocks[i].blockIsFree) {
-            void* currentEnd = (char*)blocks[i].ptrToData + blocks[i].blockSize;
-            if (blocks[i + 1].blockIsFree &&
-                (char*)blocks[i + 1].ptrToData == currentEnd) {
-                blocks[i].blockSize += blocks[i + 1].blockSize;
-                for (int j = i + 1; j < numberOfBlocks - 1; j++) {
-                    blocks[j] = blocks[j + 1];
+    for (int ia = 0; ia < numberOfBlocks - 1;) {
+        if (blocks[ia].blockIsFree) {
+            void* currentEnd = (char*)blocks[ia].ptrToData + blocks[ia].blockSize;
+            if (blocks[ia + 1].blockIsFree &&
+                (char*)blocks[ia + 1].ptrToData == currentEnd) {
+                blocks[ia].blockSize += blocks[ia + 1].blockSize;
+                for (int ja = ia + 1; ja < numberOfBlocks - 1; ja++) {
+                    blocks[ja] = blocks[ja + 1];
                 }
                 numberOfBlocks--;
             } else {
-                i++;
+                ia++;
             }
         } else {
-            i++;
+            ia++;
         }
     }
 }
@@ -43,23 +57,23 @@ void* xmalloc(size_t blockSize) {
 
     // First, Loop over our blocks, if we find a block that is a. Free and b.
     // the exact size we are looking for, we will return it.
-    for (int i = 0; i < numberOfBlocks; i++) {
-        if (blocks[i].blockIsFree && blocks[i].blockSize == blockSize) {
-            blocks[i].blockIsFree = false;
-            return blocks[i].ptrToData;
+    for (int ib = 0; ib < numberOfBlocks; ib++) {
+        if (blocks[ib].blockIsFree && blocks[ib].blockSize == blockSize) {
+            blocks[ib].blockIsFree = false;
+            return blocks[ib].ptrToData;
         }
     }
 
     // If there are no exact matches, we will look for a larger block, and split
     // it into two. One block of the desired size, and another block of the
     // remaining size.
-    for (int i = 0; i < numberOfBlocks; i++) {
-        if (blocks[i].blockIsFree && blocks[i].blockSize > blockSize) {
-            void* newPtrToData = blocks[i].ptrToData;
-            size_t remainingBlockSize = blocks[i].blockSize - blockSize;
+    for (int ic = 0; ic < numberOfBlocks; ic++) {
+        if (blocks[ic].blockIsFree && blocks[ic].blockSize > blockSize) {
+            void* newPtrToData = blocks[ic].ptrToData;
+            size_t remainingBlockSize = blocks[ic].blockSize - blockSize;
 
-            blocks[i].blockSize = blockSize;
-            blocks[i].blockIsFree = false;
+            blocks[ic].blockSize = blockSize;
+            blocks[ic].blockIsFree = false;
 
             if (numberOfBlocks >= MAX) {
                 fprintf(stderr,
@@ -81,20 +95,16 @@ void* xmalloc(size_t blockSize) {
     // this block to our existing list of blocks and return the pointer to the
     // data.
 
-    void* newPtrToData = sbrk(blockSize);
-    blocks[numberOfBlocks].ptrToData = newPtrToData;
-    blocks[numberOfBlocks].blockSize = blockSize;
-    blocks[numberOfBlocks].blockIsFree = false;
-    numberOfBlocks++;
+    void* newPtrToData = getLargerBlock(blockSize);
     return newPtrToData;
 }
 
 void xfree(void* ptrToData) {
     // Loop over all the blocks, if the desired pointer is found, and in use,
     // free it and return.
-    for (int i = 0; i < numberOfBlocks; i++) {
-        if (blocks[i].ptrToData == ptrToData && !blocks[i].blockIsFree) {
-            blocks[i].blockIsFree = true;
+    for (int id = 0; id < numberOfBlocks; id++) {
+        if (blocks[id].ptrToData == ptrToData && !blocks[id].blockIsFree) {
+            blocks[id].blockIsFree = true;
             break;
         }
     }
--- malloc.h
+++ malloc.h
@@ -3,7 +3,6 @@
 #ifndef MALLOC_H
 #define MALLOC_H
 
-void mergeBlocks();
 void* xmalloc(size_t size);
 void xfree(void* ptr);
 
